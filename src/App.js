7f02feeb399f9c5d521191a4183d106f98ad619a
Kristen Prescott <prescottdarling@gmail.com>
2021-07-01 13:13:52 -0400

    added/removed cursors

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Searchbar from "./components/Searchbar/Searchbar";
 import DateTime from "./components/DateTime/DateTime";
 import WeatherWidget from "./components/WeatherWidget/WeatherWidget";
 import SilverCursor from "./assets/cursors/silver-2/SilverNormalSelect.cur";
+import WorkinginBackground from "./assets/cursors/silver-2/WorkinginBackground.cur";
+import SilverHandwriting from "./assets/cursors/silver-2/SilverHandwriting.cur";
+import SilverLinkSelect from "./assets/cursors/silver-2/SilverLinkSelect.cur";
+import SilverUnavailable from "./assets/cursors/silver-2/SilverUnavailable.cur";
+import SilverHelpSelect from "./assets/cursors/silver-2/SilverHelpSelect.cur";
 
 function App() {
   return (
-    <div className="App" style={{ cursor: `url(${SilverCursor}),auto` }}>
+    <div className="App" style={{ cursor: `url(${SilverHandwriting}),auto` }}>
       <div className="wrapper">
         <Searchbar />
         <div className="container">
